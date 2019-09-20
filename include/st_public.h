3b841ce7e714dd1bc1b3e8efb06f5e30f9f8d2e5
linkxzhou <zhoulv2000@163.com>
2019-09-20 16:41:47 +0800

    增加likely宏定义

--- include/st_public.h
+++ include/st_public.h
@@ -36,13 +36,11 @@
 #define ST_NAMESPACE_END    }
 #define ST_NAMESPACE_USING  using namespace sthread;
 
-#if defined(__APPLE__)
 #if __GNUC__ == 2 && __GNUC_MINOR__ < 96 
 #define __builtin_expect(x, expected_value) (x) 
 #endif
 #define likely(x)   __builtin_expect((x),1) 
 #define unlikely(x) __builtin_expect((x),0)
-#endif
 
 #define st_safe_delete(ptr)     do          \
     {                                       \
